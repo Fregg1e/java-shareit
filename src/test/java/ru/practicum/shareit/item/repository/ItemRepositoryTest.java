28dc441f6964e20157b83e4d774809d6257f74ea
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2023-08-07 15:09:24 +0300

    test: добавить тесты для ItemRepository

--- src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.request.repository;
+package ru.practicum.shareit.item.repository;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
@@ -15,12 +16,13 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-class ItemRequestRepositoryTest {
+class ItemRepositoryTest {
     @Autowired
     private TestEntityManager em;
     @Autowired
-    private ItemRequestRepository repository;
+    private ItemRepository repository;
     private List<User> users;
+    private List<Item> items;
     private List<ItemRequest> requests;
 
     @BeforeEach
@@ -43,24 +45,42 @@ class ItemRequestRepositoryTest {
         for (ItemRequest itemRequest : requests) {
             em.persist(itemRequest);
         }
+        items = List.of(
+                Item.builder().name("1").description("1").available(true).owner(users.get(0)).build(),
+                Item.builder().name("1").description("2").available(true).owner(users.get(1))
+                        .request(requests.get(0)).build(),
+                Item.builder().name("2").description("1").available(true).owner(users.get(1)).build()
+        );
+        for (Item item : items) {
+            em.persist(item);
+        }
     }
 
     @Test
-    void findByRequestorIdOrderByCreatedDescTest() {
-        List<ItemRequest> itemRequests = repository.findByRequestorIdOrderByCreatedDesc(users.get(1).getId());
-
-        assertEquals(2, itemRequests.size());
-        assertEquals(requests.get(2), itemRequests.get(0));
-        assertEquals(requests.get(1), itemRequests.get(1));
-    }
-
-    @Test
-    void findByRequestorIdNotOrderByCreatedDescTest() {
-        List<ItemRequest> itemRequests = repository.findByRequestorIdNotOrderByCreatedDesc(users.get(0).getId(),
+    void findByOwnerIdTest() {
+        List<Item> itemByOwner = repository.findByOwnerId(users.get(1).getId(),
                 PageRequest.of(0, 3)).getContent();
 
-        assertEquals(2, itemRequests.size());
-        assertEquals(requests.get(2), itemRequests.get(0));
-        assertEquals(requests.get(1), itemRequests.get(1));
+        assertEquals(2, itemByOwner.size());
+        assertEquals(items.get(1), itemByOwner.get(0));
+        assertEquals(items.get(2), itemByOwner.get(1));
+    }
+
+    @Test
+    void searchTest() {
+        List<Item> itemBySearch = repository.search("2",
+                PageRequest.of(0, 3)).getContent();
+
+        assertEquals(2, itemBySearch.size());
+        assertEquals(items.get(1), itemBySearch.get(0));
+        assertEquals(items.get(2), itemBySearch.get(1));
+    }
+
+    @Test
+    void findByRequestIdTest() {
+        List<Item> itemByRequest = repository.findByRequestId(requests.get(0).getId());
+
+        assertEquals(1, itemByRequest.size());
+        assertEquals(items.get(1), itemByRequest.get(0));
     }
 }
